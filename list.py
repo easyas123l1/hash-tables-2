d98f1845b3c6bd008d9ca647ae108bcb2e0f2a17
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-06-02 14:46:35 -0500

    update learning material

--- list.py
+++ list.py
@@ -8,6 +8,16 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __str__(self):
+        r = ""
+        cur = self.head
+        while cur is not None:
+            r += f'({cur.value})'
+            if cur.next is not None:
+                r += ' -> '
+            cur = cur.next
+        return r
+
     def insert_at_head(self, node):
         node.next = self.head
         self.head = node
