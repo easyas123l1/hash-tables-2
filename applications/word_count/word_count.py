55a24b25ff7cf356171b2400adb58513723d5a96
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-06-03 19:04:56 -0500

    complete word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,25 @@
-def word_count(s):
-    # Your code here
 
+
+def word_count(s):
+
+    cache = {}
+    invalid = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\',
+               '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    for x in invalid:
+        s = s.replace(x, '')
+    s = s.lower()
+    s = s.split()
+    for letter in s:
+        if letter in cache:
+            cache[letter] += 1
+        else:
+            cache[letter] = 1
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
